bc42e13a7404718ce3b5cb1112a4cf90d6b40d1a
Dmytro <yakshinex@gmail.com>
2025-06-20 07:02:45 +0300

    Added movement, turning, groups

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        pygame.Surface.fill(screen, "black")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    while True: #main game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        updatable.update(dt)
+
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__=="__main__":
